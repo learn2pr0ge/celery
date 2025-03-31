5db610696b720b51dac9050df654881c99b27bdc
e@estateone.co <e@estateone.co>
2025-03-31 17:42:22 +0100

    Django logging 3

--- news_portal_dev/admin.py
+++ news_portal_dev/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Post, PostCategory, Comment
 
+class ProductAdmin(admin.ModelAdmin):
+    # list_display — это список или кортеж со всеми полями, которые вы хотите видеть в таблице с товарами
+    list_display = ('title', 'content', 'author','post_type', 'timestamp')  # генерируем список имён всех полей для более красивого отображения
+    list_filter = ('post_type', 'category')
+    search_fields = ('title', 'content')
 
-admin.site.register(Post)
+
+admin.site.register(Post, ProductAdmin)
 admin.site.register(PostCategory)
 admin.site.register(Comment)
 # Register your models here.
